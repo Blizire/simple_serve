c585760ac11803a3bfc03d9e0af8b2fe4c95b232
Trenton Stiles <Stiles.Trenton@gmail.com>
2020-03-09 23:34:24 -0600

    Add files via upload

---
+++ start.ps1
@@ -0,0 +1,6 @@
+py -3 -m venv venv
+./venv/Scripts/activate
+# if this doesnt start successfully
+# must install dependencies in virtual environment
+# pip install flask virtualenv
+py src/__main__.py
